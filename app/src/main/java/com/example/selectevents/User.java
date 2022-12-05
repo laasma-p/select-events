57ba814c4ae0ec22f1b2f0cec14a58b6113dcdeb
Lasma Poksane <laasmapoksane@gmail.com>
2022-12-05 23:18:03 +0100

    add User class for firebase needs

---
+++ app/src/main/java/com/example/selectevents/User.java
@@ -0,0 +1,14 @@
+package com.example.selectevents;
+
+public class User {
+    public String firstName, lastName, email, phoneNumber;
+
+    public User() {}
+
+    public User(String firstName, String lastName, String email, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+}
