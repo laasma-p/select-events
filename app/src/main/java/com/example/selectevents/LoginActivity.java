42de316ec9d00f840c38dbec5fde4801fb0cd9de
Lasma Poksane <laasmapoksane@gmail.com>
2022-12-06 12:46:51 +0100

    add logging in functionality to the app

--- app/src/main/java/com/example/selectevents/LoginActivity.java
+++ app/src/main/java/com/example/selectevents/LoginActivity.java
@@ -2,13 +2,85 @@ package com.example.selectevents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText email, password;
+
+    private FirebaseAuth mAuth;
+    private ProgressBar loginProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(this);
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+
+        loginProgressBar = findViewById(R.id.loginProgressBar);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        userLogin();
+    }
+
+    private void userLogin() {
+        String emailString = email.getText().toString().trim();
+        String passwordString = password.getText().toString().trim();
+
+        if (emailString.isEmpty()) {
+            email.setError("Email is required");
+            email.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailString).matches()) {
+            email.setError("Please enter a valid email");
+            email.requestFocus();
+            return;
+        }
+
+        if (passwordString.isEmpty()) {
+            password.setError("Password is required");
+            password.requestFocus();
+            return;
+        }
+
+        if (passwordString.length() < 6) {
+            password.setError("Minimum password length is 6 characters");
+            password.requestFocus();
+            return;
+        }
+
+        loginProgressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                // Redirect to HomeScreenActivity
+                startActivity(new Intent(LoginActivity.this, HomeScreenActivity.class));
+            } else {
+                Toast.makeText(LoginActivity.this,
+                        "Failed to log in. Please check your credentials",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
 }
