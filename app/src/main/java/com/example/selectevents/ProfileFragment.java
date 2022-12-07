c82f4db1753db6eb1fe830087e48bde7c2967f02
Lasma Poksane <laasmapoksane@gmail.com>
2022-12-07 03:29:43 +0100

    display data from Firebase in a profile

--- app/src/main/java/com/example/selectevents/ProfileFragment.java
+++ app/src/main/java/com/example/selectevents/ProfileFragment.java
@@ -3,6 +3,7 @@ package com.example.selectevents;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -10,18 +11,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileFragment extends Fragment {
     private static final String TAG = ".ProfileFragment";
-
-    private DatabaseReference reference;
-
-    private String userID;
 
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
@@ -44,7 +46,44 @@ public class ProfileFragment extends Fragment {
             FirebaseAuth.getInstance().signOut();
         });
 
+        // For displaying the data from Firebase
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        assert user != null;
+        String userID = user.getUid();
+
+        final TextView firstNameTextView = (TextView) view.findViewById(R.id.firstName);
+        final TextView lastNameTextView = (TextView) view.findViewById(R.id.lastName);
+        final TextView emailTextView = (TextView) view.findViewById(R.id.email);
+        final TextView phoneNumberTextView = (TextView) view.findViewById(R.id.phoneNumber);
+
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                User userProfile = snapshot.getValue(User.class);
+
+                if (userProfile != null) {
+                    String firstName = userProfile.firstName;
+                    String lastName = userProfile.lastName;
+                    String email = userProfile.email;
+                    String phoneNumber = userProfile.phoneNumber;
+
+                    firstNameTextView.setText(firstName);
+                    lastNameTextView.setText(lastName);
+                    emailTextView.setText(email);
+                    phoneNumberTextView.setText(phoneNumber);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(), "Cannot display data", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return view;
     }
