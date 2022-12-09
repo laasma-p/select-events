c9f2221ed6f4f851ad8dd51d72a839800196aa1f
Lasma Poksane <laasmapoksane@gmail.com>
2022-12-09 14:28:55 +0100

    prevent back button to be pressed to not log out

--- app/src/main/java/com/example/selectevents/EventsFragment.java
+++ app/src/main/java/com/example/selectevents/EventsFragment.java
@@ -1,14 +1,21 @@
 package com.example.selectevents;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class EventsFragment extends Fragment {
+    private FirebaseAuth mAuth;
+
     public EventsFragment() {
         // Required empty public constructor
     }
@@ -16,7 +23,27 @@ public class EventsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_events, container, false);
     }
+
+    // Preventing back button to not log out of the app
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        OnBackPressedCallback callback = new OnBackPressedCallback(
+                true // default to enabled
+        ) {
+            @Override
+            public void handleOnBackPressed() {
+                // Do nothing
+            }
+        };
+
+        requireActivity().getOnBackPressedDispatcher().addCallback(
+                this, // LifecycleOwner
+                callback);
+    }
+
 }
