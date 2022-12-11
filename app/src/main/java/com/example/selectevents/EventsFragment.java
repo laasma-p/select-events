ab3a52542edbf3b4de66be62ac9072bfaee4d0ad
Lasma Poksane <laasmapoksane@gmail.com>
2022-12-11 23:00:57 +0100

    display events from Firebase

--- app/src/main/java/com/example/selectevents/EventsFragment.java
+++ app/src/main/java/com/example/selectevents/EventsFragment.java
@@ -1,20 +1,38 @@
 package com.example.selectevents;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class EventsFragment extends Fragment {
-    private FirebaseAuth mAuth;
+    RecyclerView eventList;
+    EventAdapter eventAdapter;
+
+    FirebaseAuth mAuth;
+
+    DatabaseReference reference;
+    ArrayList<Event> eventArrayList;
 
     public EventsFragment() {
         // Required empty public constructor
@@ -23,9 +41,43 @@ public class EventsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events, container, false);
+        View view = inflater.inflate(R.layout.fragment_events, container, false);
+
+        // Finding Recycler View
+        eventList = view.findViewById(R.id.rv);
+
+        reference = FirebaseDatabase.getInstance().getReference("events");
+
+        eventList.hasFixedSize();
+
+        eventList.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        eventArrayList = new ArrayList<>();
+        eventAdapter = new EventAdapter(getActivity(), eventArrayList);
+
+        eventList.setAdapter(eventAdapter);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+
+                    Event event = dataSnapshot.getValue(Event.class);
+                    eventArrayList.add(event);
+                }
+
+                eventAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return view;
     }
 
     // Preventing back button to not log out of the app
