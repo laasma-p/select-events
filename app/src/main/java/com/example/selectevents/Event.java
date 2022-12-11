38cf6c0e586cdb1f977f60099e3c0dc3326a2cb9
Lasma Poksane <laasmapoksane@gmail.com>
2022-12-11 01:36:13 +0100

    create initial Event class

---
+++ app/src/main/java/com/example/selectevents/Event.java
@@ -0,0 +1,48 @@
+package com.example.selectevents;
+
+public class Event {
+
+    String title, description, time, date;
+
+
+    public Event() {}
+
+    public Event(String title, String description, String time, String date) {
+        this.title = title;
+        this.description = description;
+        this.time = time;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
