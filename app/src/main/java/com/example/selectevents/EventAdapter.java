43b695bd9234d8b9ce1e82160f2d818c2050ecb9
Lasma Poksane <laasmapoksane@gmail.com>
2022-12-13 17:38:20 +0100

    display RecycleView items via an adapter

--- app/src/main/java/com/example/selectevents/EventAdapter.java
+++ app/src/main/java/com/example/selectevents/EventAdapter.java
@@ -1,6 +1,7 @@
 package com.example.selectevents;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,59 +10,68 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
+import com.google.android.material.card.MaterialCardView;
+
+import java.util.List;
 
 public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder> {
 
-    Context context;
-    ArrayList<Event> eventArrayList;
+    private final Context context;
+    private final List<Event> eventList;
 
-    public EventAdapter(Context context, ArrayList<Event> eventArrayList) {
+    public EventAdapter(Context context, List<Event> eventList) {
         this.context = context;
-        this.eventArrayList = eventArrayList;
+        this.eventList = eventList;
     }
 
     @NonNull
     @Override
     public EventAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.event_card, parent, false);
-        return new ViewHolder(view);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.event_card, parent, false);
+
+        return new EventAdapter.ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull EventAdapter.ViewHolder holder, int position) {
-        Event event = eventArrayList.get(position);
+        holder.title.setText(eventList.get(position).getTitle());
+        holder.description.setText(eventList.get(position).getDescription());
+        holder.time.setText(eventList.get(position).getTime());
+        holder.date.setText(eventList.get(position).getDate());
 
-        holder.title.setText(event.getTitle());
-        holder.description.setText(event.getDescription());
-        holder.time.setText(event.getTime());
-        holder.date.setText(event.getDate());
+        holder.card.setOnClickListener(v -> {
+            Intent intent = new Intent(context, EventDetailsActivity.class);
+
+            intent.putExtra("Title", eventList.get(holder.getAdapterPosition()).getTitle());
+            intent.putExtra("Description", eventList.get(holder.getAdapterPosition()).getDescription());
+            intent.putExtra("Time", eventList.get(holder.getAdapterPosition()).getTime());
+            intent.putExtra("Date", eventList.get(holder.getAdapterPosition()).getDate());
+
+            context.startActivity(intent);
+        });
+
     }
 
     @Override
     public int getItemCount() {
-        return eventArrayList.size();
+        return eventList.size();
     }
-
 
     static class ViewHolder extends RecyclerView.ViewHolder {
+
         TextView title, description, time, date;
+        MaterialCardView card;
 
         ViewHolder(View itemView) {
+
             super(itemView);
 
-            title = itemView.findViewById(R.id.rvEventTitle);
-            description = itemView.findViewById(R.id.rvEventDescription);
-            time = itemView.findViewById(R.id.rvEventTime);
-            date = itemView.findViewById(R.id.rvEventDate);
+            card = itemView.findViewById(R.id.card);
+            title = itemView.findViewById(R.id.eventTitle);
+            description = itemView.findViewById(R.id.eventDescription);
+            time = itemView.findViewById(R.id.eventTime);
+            date = itemView.findViewById(R.id.eventDate);
         }
     }
-
-
-
-
-
-
-
 
 }
