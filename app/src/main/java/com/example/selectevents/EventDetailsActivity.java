52e5b5d1c0d844c9e5aefdd9a99a72b7a4dbc66d
Lasma Poksane <laasmapoksane@gmail.com>
2022-12-15 03:20:26 +0100

    add Dialog to be able to delete an event

--- app/src/main/java/com/example/selectevents/EventDetailsActivity.java
+++ app/src/main/java/com/example/selectevents/EventDetailsActivity.java
@@ -2,6 +2,7 @@ package com.example.selectevents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +10,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -48,13 +48,34 @@ public class EventDetailsActivity extends AppCompatActivity {
             id = bundle.getString("EventId");
         }
 
-        deleteEventButton.setOnClickListener(v -> {
-            reference = FirebaseDatabase.getInstance().getReference("events").child(user.getUid()).child(id);
+        deleteEventButton.setOnClickListener(v -> deleteEvent(id));
+    }
+
+    public void deleteEvent(String eventId) {
+        reference = FirebaseDatabase.getInstance().getReference("events").child(user.getUid()).child(id);
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(EventDetailsActivity.this);
+        dialog.setTitle("Are you sure?");
+        dialog.setMessage("Deleting the event is irreversible.");
+        dialog.setPositiveButton("Delete", (dialog1, which) -> {
+            deleteEventButton.setVisibility(View.VISIBLE);
+            assert user != null;
+
             reference.removeValue();
             Toast.makeText(EventDetailsActivity.this,"Event Deleted", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(getApplicationContext(), HomeScreenActivity.class);
+
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+
             finish();
         });
 
+        dialog.setNegativeButton("Dismiss", (dialog12, which) -> dialog12.dismiss());
+
+        AlertDialog alertDialog = dialog.create();
+        alertDialog.show();
     }
 
 }
